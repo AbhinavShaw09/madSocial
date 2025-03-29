e2ef3eff7e4b4380fc880656996bb09f11007ff7
Abhinav <shawabhinav93@gmail.com>
2025-03-29 09:44:46 +0530

    chaged the navbar

--- core/settings.py
+++ core/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-52t^yl49r)=gts^@+j1p(ybtuy6a*f7_@vdyf_-=c!i-7%v=ll"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
