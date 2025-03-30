0104a0e6967ca0a104dafd0fcf29c12dc8a35a7c
Abhinav <shawabhinav93@gmail.com>
2025-03-30 18:50:33 +0530

    minor changes to navbar and started with comments feature

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-52t^yl49r)=gts^@+j1p(ybtuy6a*f7_@vdyf_-=c!i-7%v=ll"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
